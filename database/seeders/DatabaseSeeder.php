d1ca49f6a18e082b83dca8e9643291d0a8608762
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-19 03:19:25 -0400

    Completado 9/31

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        \App\Models\User::factory()->create();
+        \App\Models\Post::factory(80)->create();
     }
 }
