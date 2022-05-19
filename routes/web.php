ebdd034b153a59ef756453c87f22e9c0c40c5e81
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-19 02:44:22 -0400

    Completado 7/31 Controladores:Agrupado rutas

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,16 @@ use Illuminate\Support\Facades\Route;
  * Route::delete    | Eliminar
  * Route::put       | Actualizar
  */
-Route::get('/', [PageController::class, 'home'])->name('home');
+/**
+ * Route::get('/', [PageController::class, 'home'])->name('home');
+ * Route::get('blog', [PageController::class, 'blog'])->name('blog');
+ * Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
+ */
 
-Route::get('blog', [PageController::class, 'blog'])->name('blog');
+Route::controller(PageController::class)->group(function() {
+    Route::get('/', 'home')->name('home');
+    Route::get('blog', 'blog')->name('blog');
+    Route::get('blog/{slug}', 'post')->name('post');
+});
 
-Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
 
