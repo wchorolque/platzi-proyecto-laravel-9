b06103ff3c0faa1e8b211305118a3eca5d8fa082
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-19 01:47:53 -0400

    Completado 4/31

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+/**
+ * Route::get       | Consultar
+ * Route::post      | Guardar
+ * Route::delete    | Eliminar
+ * Route::put       | Actualizar
+ */
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta home';
+    //return view('welcome');
 });
+
+Route::get('blog', function () {
+    return 'Lista de publicaciones';
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    // consulta a base de datos
+    return $slug;
+});
+
+Route::get('buscar', function(Request $request){
+    return $request->all();
+});
+
