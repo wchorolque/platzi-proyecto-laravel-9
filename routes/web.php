50eca29fc7a174a973d2967f4350dc231186a747
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-19 02:19:57 -0400

    Completado 6/31

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
  */
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('blog', function () {
     $posts = [
@@ -20,13 +20,13 @@ Route::get('blog', function () {
     ];
 
     return view('blog', ['posts' => $posts]);
-});
+})->name('blog');
 
 Route::get('blog/{slug}', function ($slug) {
     $post = $slug;
 
     return view('post', ['post' => $post]);
-});
+})->name('post');
 
 Route::get('buscar', function(Request $request){
     return $request->all();
