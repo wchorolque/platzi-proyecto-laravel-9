00b15a7b79012b8b5b418002dce66fc95e6d74cd
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-22 23:54:21 -0400

    Completado 18/31

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight flex items-center justify-between">
             {{ __('Posts') }}
-            <a href="{{ route('posts.create') }}">Crear</a>
+            <a href="{{ route('posts.create') }}" class="text-xs bg-gray-800 text-white rounded px-2 py-1">Crear</a>
         </h2>
     </x-slot>
 
