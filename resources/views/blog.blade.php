08282e7567f30c78dc38dc769566953a3566ef7f
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-22 22:28:57 -0400

    Completado 11/31

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -9,6 +9,8 @@
         <a href="{{ route('post', $post->slug) }}">
             {{ $post->title }}
         </a>
+        <br/>
+        <span>{{ $post->user->name }}</span>
     </p>
     @endforeach
 
