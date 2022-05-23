c5a9b84440303cf3d32c2b5c055e2da2c785b365
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-22 23:34:01 -0400

    Completado 16/31

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,28 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                Listado de publicaciones
+
+                <table class="mb-4">
+                    <thead>
+                        <tr>
+                            <th>TITULO</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($posts as $post)
+                        <tr class="border-b border-gray-200 text-sm">
+                            <td class="px-6 py-4">{{ $post->title }}</td>
+                            <td class="px-6 py-4"><a href="" class="text-indigo-600">editar</a></td>
+                            <td class="px-6 py-4"><a href="" class="text-indigo-600">eliminar</a></td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+                {{ $posts->links() }}
+
                 </div>
             </div>
         </div>
