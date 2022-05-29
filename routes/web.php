5c4d55689d0a78beb68757154e988b5c643b71c7
Williams Israel Chorolque Choque <wchorolque@gmail.com>
2022-05-28 21:20:45 -0400

    Completado 26/31

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(PageController::class)->group(function() {
     Route::get('/', 'home')->name('home');
-    Route::get('blog', 'blog')->name('blog');
     Route::get('blog/{post:slug}', 'post')->name('post');
 });
 
